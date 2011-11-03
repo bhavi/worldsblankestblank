ec4b2495962287d1cd9387888ba1fa50c8c6adfe
Micah Cooper <mrmicahcooper@gmail.com>
2011-11-03 00:06:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Worldsblankestblank::Application.config.secret_token = '665a70d5d9c75ecc513633e03fefcb3affb8b32c87630f1deb04e10384237370bf0b82b3861a077d3b228530500ccd1ec8b49cc8cd6f464365796464dead00ca'
